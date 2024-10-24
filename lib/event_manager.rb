fc80da863c55a1cc362875015e37fd01445c6362
MJ12X <179878352+MJ12X@users.noreply.github.com>
2024-10-24 16:54:57 +0100

    created a way to vailidate whether or not the code is 5 digits, less than five digits or more than five digits and appropriately dealt with the issue. an error message comes up on length for some values still.

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -10,6 +10,16 @@ contents = CSV.open(
 contents.each do |row|
   name = row[:first_name]
   zipcode = row[:zipcode]
+
+  #if the zipcode is 5 digits assume its okay
+  #if the zipcode is less than five digits, add zeros to the front until it becomes five digits.
+  if zipcode.length < 5
+    zipcode = zipcode.rjust(5, '0')
+    #if the zipcode is more than five digits, truncate it to the first five digits
+  elsif zipcode.length > 5
+    zipcode = zipcode[0..4]
+  end
+  
   puts "#{name} #{zipcode}"
 end
 
