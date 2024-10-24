7cb9c93f377c780121dcde0f9be954a1b3af3e0d
MJ12X <179878352+MJ12X@users.noreply.github.com>
2024-10-24 16:21:06 +0100

    iterate through the data set and puts each line one after another.

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,5 +1,8 @@
 puts "Event Manager Initialized!"
 File.exist? "event_attendees.csv"
 
-contents = File.read(event_attendees.csv)
-puts contents
+lines = File.readlines('event_attendees.csv')
+lines.each do |line|
+  puts line
+end
+
