bb3d93b386e4ae6651a7211e46a9ad56b252afb2
MJ12X <179878352+MJ12X@users.noreply.github.com>
2024-10-24 17:00:08 +0100

    no value zipcodes were messing up the iterating process. when evaluating each element, it will return nil if there is nothing there so i checked for nil values and gave a set zipcode.

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -12,8 +12,11 @@ contents.each do |row|
   zipcode = row[:zipcode]
 
   #if the zipcode is 5 digits assume its okay
+  #if zipcode isn't there it will output nil, we must check for this and give it a default value.
+  if zipcode.nil?
+    zipcode = '00000'
   #if the zipcode is less than five digits, add zeros to the front until it becomes five digits.
-  if zipcode.length < 5
+  elsif zipcode.length < 5
     zipcode = zipcode.rjust(5, '0')
     #if the zipcode is more than five digits, truncate it to the first five digits
   elsif zipcode.length > 5
