ad2f369066c2c6c568368a30347f43047042041e
MJ12X <179878352+MJ12X@users.noreply.github.com>
2024-10-24 18:58:29 +0100

    made it so i can access the legislators name and have it output in a string format rather than in an array.

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -27,10 +27,17 @@ contents.each do |row|
     levels: 'country',
     roles: ['legislatorUpperBody', 'legislatorLowerBody']
   )
+
   legislators = legislators.officials
+
+    legislator_names = legislators.map(&:name)
+
+    legislators_string = legislator_names.join(", ")
+  
   rescue
     'you can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
-  puts "#{name} #{zipcode} #{legislators}"
+
+  puts "#{name} #{zipcode} #{legislators_string}"
 end
 
