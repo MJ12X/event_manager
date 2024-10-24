15f8dec6762d89a702f35082aa8612244157b180
MJ12X <179878352+MJ12X@users.noreply.github.com>
2024-10-24 16:34:30 +0100

    it removes the first_name header from the outputted list while also being able to stay in the first_name column as we have indexed it to check for this line and skip if not on this line.

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -2,8 +2,10 @@ puts "Event Manager Initialized!"
 File.exist? "event_attendees.csv"
 
 lines = File.readlines('event_attendees.csv')
-lines.each do |line|
+lines.each_with_index do |line,index|
+  next if index == 0
   columns = line.split(",")
-  p columns
+  name = columns[2]
+  puts name
 end
 
