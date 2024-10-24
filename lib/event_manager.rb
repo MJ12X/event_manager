e74b2ea951e81d4ce26748cfda5029f23ec276bb
MJ12X <179878352+MJ12X@users.noreply.github.com>
2024-10-24 17:22:36 +0100

    installed the Google API client and used the documentation to use it by setting the API key, sending the query with the given information and then parsing the response of the names of the legislators.

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,21 +1,33 @@
-puts "Event Manager Initialized!"
-require "csv"
+require 'csv'
+require 'google/apis/civicinfo_v2'
+
+civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
 def clean_zipcode(zipcode)
-zipcode.to_s.rjust(5, '0')[0..4]
+  zipcode.to_s.rjust(5, '0')[0..4]
 end
 
+puts 'EventManager initialized.'
+
 contents = CSV.open(
-  'event_attendees.csv', 
+  'event_attendees.csv',
   headers: true,
   header_converters: :symbol
-  )
+)
 
 contents.each do |row|
   name = row[:first_name]
 
   zipcode = clean_zipcode(row[:zipcode])
 
-  puts "#{name} #{zipcode}"
+  legislators = civic_info.representative_info_by_address(
+    address: zipcode,
+    levels: 'country',
+    roles: ['legislatorUpperBody', 'legislatorLowerBody']
+  )
+  legislators = legislators.officials
+
+  puts "#{name} #{zipcode} #{legislators}"
 end
 
